7155576a960c0b7d592ec696004a4c6f91823c83
Digvijay <digvijaybakare816@gmail.com>
2024-12-30 10:11:19 +0530

    Spring Boot Application with H2 Database DONE!

---
+++ H2_DataBase_Demo/src/main/java/com/h2db/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.h2db.repository;
+
+import com.h2db.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
